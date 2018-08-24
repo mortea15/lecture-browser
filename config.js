cff9d3434cf8c2efa79426f5b4443affe00de2a6
mortea15 <mortea15@student.uia.no>
2018-08-24 17:53:41 +0200

    added list of allowed filetypes

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
-var config = {};
+var config = {}
 
-config.dbstring = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_COLL}`;
+config.dbstring = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_COLL}`
+config.fileTypes = ['text/markdown']
 
-module.exports = config;
+module.exports = config
