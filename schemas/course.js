ef803acfe5bf32364483a2f8c51a885405237e05
mortea15 <mortea15@student.uia.no>
2018-08-24 15:13:47 +0200

    mongo schema for courses

---
+++ schemas/course.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+
+// Schema for use with mongoose
+var CourseSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    code: {
+        type: Number,
+        required: true
+    },
+    title: {
+        type: String
+    },
+    lectures: {
+        type: [mongoose.Schema.Types.ObjectId],
+        required: false
+    },
+    semester: {
+        type: String,
+        required: false
+    }
+}, { timestamps: true });
+
+module.exports = CourseSchema;
