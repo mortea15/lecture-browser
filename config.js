c6aa93c9bb5f88864c90dd2bf78d86771e7819d1
mortea15 <mortea15@student.uia.no>
2018-08-24 14:49:15 +0200

    config file

---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {};
+
+config.dbstring = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_COLL}`;
+
+module.exports = config;
