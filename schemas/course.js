876f9bf562dca4612ba54868586e9372a6c4b560
mortea15 <mortea15@student.uia.no>
2018-08-24 15:13:56 +0200

    eslint

--- schemas/course.js
+++ schemas/course.js
@@ -1,26 +1,26 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose')
 
 // Schema for use with mongoose
 var CourseSchema = new mongoose.Schema({
-    id: {
-        type: String,
-        required: true
-    },
-    code: {
-        type: Number,
-        required: true
-    },
-    title: {
-        type: String
-    },
-    lectures: {
-        type: [mongoose.Schema.Types.ObjectId],
-        required: false
-    },
-    semester: {
-        type: String,
-        required: false
-    }
-}, { timestamps: true });
+  id: {
+    type: String,
+    required: true
+  },
+  code: {
+    type: Number,
+    required: true
+  },
+  title: {
+    type: String
+  },
+  lectures: {
+    type: [mongoose.Schema.Types.ObjectId],
+    required: false
+  },
+  semester: {
+    type: String,
+    required: false
+  }
+}, { timestamps: true })
 
-module.exports = CourseSchema;
+module.exports = CourseSchema
