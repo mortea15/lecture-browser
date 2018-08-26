42833693ccb271c8d10403969f2c14fe500a3840
mortea15 <mortea15@student.uia.no>
2018-08-26 23:38:54 +0200

    use appname from config

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express')
 var router = express.Router()
 
+const config = require('../config')
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   if (req.session.token) {
     res.redirect('/courses/view')
   } else {
-    res.render('index', { title: 'Lecture Browser' })
+    res.render('index', { title: config.appname })
   }
 })
 
