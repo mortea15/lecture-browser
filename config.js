ee6e7d7a13c42df7930c7c51490cf5b32eb0fd7e
mortea15 <mortea15@student.uia.no>
2018-08-26 22:10:59 +0200

    host url for axios

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ var config = {}
 config.dbstring = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_COLL}`
 config.fileTypes = ['text/markdown']
 config.appsecret = process.env.APP_SECRET
+config.host = process.env.HOST
 
 module.exports = config
