73a03a0bce5e1a262c1d0fe697823130ecd07dee
mortea15 <mortea15@student.uia.no>
2018-08-26 23:38:06 +0200

    added common appname

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 var config = {}
 
+config.appname = 'Lecture Browser'
 config.dbstring = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_COLL}`
 config.fileTypes = ['text/markdown']
 config.appsecret = process.env.APP_SECRET
