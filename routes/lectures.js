5fb512c83466172a71572b20ac842a00009f84ed
mortea15 <mortea15@student.uia.no>
2018-08-29 12:28:54 +0200

    added routes to edit lecture notes and save updated lecture notes

--- routes/lectures.js
+++ routes/lectures.js
@@ -2,6 +2,7 @@ var express = require('express')
 var router = express.Router()
 var md = require('node-markdown').Markdown
 var mongoose = require('mongoose')
+var fs = require('fs')
 
 const config = require('../config')
 const LectureSchema = require('../schemas/lecture')
@@ -122,7 +123,7 @@ router.get('/:id', function (req, res, next) {
 })
 
 /* GET lecture by object ID render */
-router.get('/view/:id', function (req, res, next) {
+router.get('/:id/view', function (req, res, next) {
   var lectureId = req.sanitize(req.params.id)
 
   mongoose.connect(config.dbstring)
@@ -153,7 +154,6 @@ router.get('/view/:id', function (req, res, next) {
               l.course_details = details
 
               // Get the file containing the notes
-              var fs = require('fs')
               var path = require('path')
               var dir = path.join(__dirname, '../')
               var filePath = path.join(dir, l.filePath)
@@ -181,6 +181,97 @@ router.get('/view/:id', function (req, res, next) {
   })
 })
 
+/* GET edit lecture by object ID render */
+router.get('/:id/edit', function (req, res, next) {
+  var lectureId = req.sanitize(req.params.id)
+
+  mongoose.connect(config.dbstring)
+    .catch((err) => {
+      console.log('DB connection error', err)
+    })
+
+  Lecture.findById(lectureId, function (err, lecture) {
+    if (err) {
+      res.status(500).end()
+    } else {
+      if (lecture) {
+        var courseId = lecture.course
+        Course.findById(courseId, function (err, course) {
+          if (err) {
+            res.status(500).end()
+          } else {
+            if (course) {
+              var l = lecture.toObject()
+              var c = course.toObject()
+              var details = {
+                _id: c._id,
+                id: c.id,
+                code: c.code,
+                title: c.title,
+                semester: c.semester
+              }
+              l.course_details = details
+
+              // Get the file containing the notes
+              var path = require('path')
+              var dir = path.join(__dirname, '../')
+              var filePath = path.join(dir, l.filePath)
+
+              var lectureDate = dateConverter(l.date)
+
+              fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
+                if (err) { throw err }
+                l.notes = data
+                var title = `Edit | Lecture ${lectureDate} in ${l.course_details.id}`
+
+                Course.find({}, function (err, courses) {
+                  if (err) {
+                    res.json({ success: false, message: 'An error occurred while connecting to the database', err })
+                  } else {
+                    res.render('edit', { title: title, lecture: l, courses: courses })
+                  }
+                })
+              })
+            } else {
+              var response = { success: false, message: 'No matching course was found' }
+              res.json(response)
+            }
+          }
+        })
+      } else {
+        var response = { success: false, message: 'No matching lecture was found' }
+        res.json(response)
+      }
+    }
+  })
+})
+
+/* POST save lecture. */
+router.post('/save', function (req, res, next) {
+  var path = require('path')
+
+  // Params
+  const course = req.sanitize(req.body.course)
+  const content = req.sanitize(req.body.content)
+  const lectureId = req.sanitize(req.body.lectureId)
+  const filePath = req.sanitize(req.body.filePath)
+
+  // File path
+  const parentFolder = path.join(__dirname, '../')
+  const fullPath = path.join(parentFolder, filePath)
+
+  if (course && content && lectureId) {
+    fs.writeFile(fullPath, content, 'utf-8', function (err) {
+      if (err) throw err
+      console.log(content.substring(0, 15))
+      console.log('file update complete')
+      res.redirect(`/lectures/${lectureId}/view`)
+    })
+  } else {
+    res.json({ success: false, message: 'Missing properties' })
+  }
+})
+
 /* GET lectures */
 router.get('/', function (req, res, next) {
   mongoose.connect(config.dbstring)
@@ -205,7 +296,6 @@ router.post('/upload', fileUpload.single('file'), function (req, res) {
   var allowed = config.fileTypes
 
   if (file && allowed.includes(file.mimetype)) {
-    var fs = require('fs')
     var readline = require('readline')
     var path = require('path')
 
